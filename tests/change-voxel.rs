a760189f4114150b1bf020f3bd8ff74cd5d3e843
Jan Ole Zabel <jan.zabel@posteo.de>
2022-10-22 23:27:28 +0200

    Add extensive tests

---
+++ tests/change-voxel.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+mod common;
+use minetestworld::{World, Position};
+
+async fn change_voxel() -> Result<(), minetestworld::world::WorldError> {
+    let world = World::new("TestWorld copy");
+    let mut vm = world.get_voxel_manip(true).await?;
+    vm.set_content(Position::new(0i16,0,0), b"default:diamond").await?;
+    vm.commit().await?;
+    std::mem::drop(vm);
+
+    let mut vm = world.get_voxel_manip(true).await?;
+    let node = vm.get_node(Position::new(0i16,0,0)).await?;
+    assert_eq!(node.param0, "default:diamond");
+    Ok(())
+}
+
+#[async_std::test]
+async fn test_change() -> Result<(), Box<dyn Error>> {
+    common::tear_up().await?;
+    // No early return here, so that tear down happens in every case
+    let result = change_voxel().await;
+    let cleanup_result = common::tear_down().await;
+    result?;
+    cleanup_result?;
+    Ok(())
+}
---
+++ tests/common/mod.rs
@@ -0,0 +1,12 @@
+use async_std::fs;
+
+pub async fn tear_up() -> std::io::Result<()> {
+    fs::create_dir("TestWorld copy").await?;
+    fs::copy("TestWorld/world.mt", "TestWorld copy/world.mt").await?;
+    fs::copy("TestWorld/map.sqlite", "TestWorld copy/map.sqlite").await?;
+    Ok(())
+}
+
+pub async fn tear_down() -> std::io::Result<()> {
+    fs::remove_dir_all("TestWorld copy").await
+}
