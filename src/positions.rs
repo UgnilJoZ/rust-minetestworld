8603bca1b5377e2776dc81443da54221ba407308
Jan Ole Zabel <jan.zabel@posteo.de>
2023-01-20 18:32:34 +0100

    Fix position conversion in postgres backend

--- src/positions.rs
+++ src/positions.rs
@@ -9,6 +9,7 @@ use sqlx::sqlite::SqliteRow;
 #[cfg(any(feature = "sqlite", feature = "postgres"))]
 use sqlx::{FromRow, Row};
 use std::ops::{Add, Rem};
+use std::io;
 
 /// A point location within the world
 ///
@@ -64,6 +65,12 @@ impl std::ops::Mul<i16> for Position {
     }
 }
 
+fn invalid_data_error<E>(error: E) -> sqlx::Error
+    where E: Into<Box<dyn std::error::Error + Send + Sync>>
+{
+    sqlx::Error::Io(io::Error::new(io::ErrorKind::InvalidData, error))
+}
+
 #[cfg(feature = "sqlite")]
 impl FromRow<'_, SqliteRow> for Position {
     fn from_row(row: &SqliteRow) -> sqlx::Result<Self> {
@@ -73,11 +80,15 @@ impl FromRow<'_, SqliteRow> for Position {
 
 #[cfg(feature = "postgres")]
 impl FromRow<'_, PgRow> for Position {
+    /// Will fail if one of the pos components do not fit in an i16
     fn from_row(row: &PgRow) -> sqlx::Result<Self> {
+        let x: i32 = row.try_get("posx")?;
+        let y: i32 = row.try_get("posy")?;
+        let z: i32 = row.try_get("posz")?;
         Ok(Position {
-            x: row.try_get("posx")?,
-            y: row.try_get("posy")?,
-            z: row.try_get("posz")?,
+            x: x.try_into().map_err(invalid_data_error)?,
+            y: y.try_into().map_err(invalid_data_error)?,
+            z: z.try_into().map_err(invalid_data_error)?,
         })
     }
 }
